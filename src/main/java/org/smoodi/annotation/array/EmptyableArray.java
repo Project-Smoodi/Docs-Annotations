34af0abb90626a4dbab4056605b9fa3ab0d18f49
Daybreak312 <ty82afg12@gmail.com>
2024-09-13 12:30:42 +0900

    FEAT :: Copied, Emptyable, Unmodifiable

--- src/main/java/org/smoodi/annotation/array/EmptyableArray.java
+++ src/main/java/org/smoodi/annotation/array/EmptyableArray.java
@@ -1,9 +1,6 @@
-package org.smoodi.annotation;
+package org.smoodi.annotation.array;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * <p>{@link java.util.Collection Collection} 혹은 {@link java.lang.reflect.Array Array} 등의 타입을 가진 메소드 반환값, 매개변수, 혹은 필드 등이 비어있을 수 있으며 비어있어도 괜찮음을 표시.</p>
@@ -19,8 +16,12 @@ import java.lang.annotation.Target;
  *     &#064;EmptyableArray
  *     public{@literal Set<String>} names ={@literal new Set<>()};
  * </pre>
+ *
+ * @author Daybreak312
+ * @since v1.0.0
  */
-@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
 @Retention(RetentionPolicy.CLASS)
+@Documented
 public @interface EmptyableArray {
 }
--- src/main/java/org/smoodi/annotation/array/UnmodifiableArray.java
+++ src/main/java/org/smoodi/annotation/array/UnmodifiableArray.java
@@ -1,2 +1,23 @@
-package org.smoodi.annotation;public @interface UnmodifiableArray {
+package org.smoodi.annotation.array;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>수정 불가능한 {@link java.util.Collection Collection} 혹은 {@link java.lang.reflect.Array Array}임을 표시.</p>
+ *
+ * <p>아래의 경우, {@link java.util.List List} 내부에서 {@link java.util.ImmutableCollections ImmutableList}를 반환하므로 해당 어노테이션을 사용함</p>
+ * <pre>
+ *     &#064UnmodifiableArray
+ *     public{@literal List<String>} getList() {
+ *         return List.of(1, 2, 3);
+ *     }
+ * </pre>
+ *
+ * @since v1.0.0
+ * @author Daybreak312
+ */
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.FIELD})
+@Retention(RetentionPolicy.CLASS)
+@Documented
+public @interface UnmodifiableArray {
 }
