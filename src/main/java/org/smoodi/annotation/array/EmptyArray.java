e731f119b1be73d280e2809da1e33d2b22b2e379
Daybreak312 <ty82afg12@gmail.com>
2024-09-15 23:56:54 +0900

    FEAT :: EmptyArray

---
+++ src/main/java/org/smoodi/annotation/array/EmptyArray.java
@@ -0,0 +1,27 @@
+package org.smoodi.annotation.array;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>비어있는 {@link java.util.Collection Collection} 혹은 {@link java.lang.reflect.Array Array}임을 표시.</p>
+ *
+ * <p>아래 경우, 반환값인 {@literal List<String>}이 비어있음을 표시함.</p>
+ * <pre>
+ *     &#064;EmptyArray
+ *     public{@literal List<String>} getNames();
+ * </pre>
+ *
+ * <p>아래 경우, 필드가 비어있음을 표시함.</p>
+ * <pre>
+ *     &#064;EmptyableArray
+ *     public{@literal Set<String>} names ={@literal new Set<>()};
+ * </pre>
+ *
+ * @author Daybreak312
+ * @since v1.0.0
+ */
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
+@Retention(RetentionPolicy.CLASS)
+@Documented
+public @interface EmptyArray {
+}
