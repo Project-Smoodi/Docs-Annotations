8f357c9314b2d335f20a5fb49101d52c2b0c586b
Daybreak312 <ty82afg12@gmail.com>
2024-09-24 11:11:46 +0900

    SNAPSHOT :: Update publishing uri

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ publishing {
     repositories {
         maven {
             name = "Docs-Annotations"
-            url = uri("https://maven.pkg.github.com/Project-Smoodi")
+            url = uri("https://maven.pkg.github.com/Project-Smoodi/Docs-Annotations")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
