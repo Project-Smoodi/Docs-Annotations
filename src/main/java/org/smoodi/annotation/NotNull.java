482c521ca0378e52dfb5a9fd864650f619bfc53a
Daybreak312 <ty82afg12@gmail.com>
2024-09-24 10:22:00 +0900

    ANNO :: 주석 작성

--- src/main/java/org/smoodi/annotation/NotNull.java
+++ src/main/java/org/smoodi/annotation/NotNull.java
@@ -2,6 +2,21 @@ package org.smoodi.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * <p>{@code null}이 아님을 표시.</p>
+ *
+ * <p>아래의 경우, 매개변수가 {@code null}일 수 없음을 표시.</p>
+ *
+ * <pre>
+ *     void run(
+ *         &#064NotNull
+ *         Object obj
+ *     );
+ * </pre>
+ *
+ * @author Daybreak312
+ * @since v1.1.0
+ */
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- src/main/java/org/smoodi/annotation/NullValue.java
+++ src/main/java/org/smoodi/annotation/NullValue.java
@@ -2,6 +2,19 @@ package org.smoodi.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * <p>{@code null}임을 표시.</p>
+ *
+ * <p>아래의 경우, 반환값이 {@code null}임을 표시.</p>
+ *
+ * <pre>
+ *     &#064NullValue
+ *     String getAuthority();
+ * </pre>
+ *
+ * @author Daybreak312
+ * @since v1.1.0
+ */
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
